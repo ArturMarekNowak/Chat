9443f568a20df2a6d57a6ea695d2f3d28238897a
Artur Nowak <arturmareknowal@gmail.com>
2023-03-18 19:24:42 +0100

    Refactor user schema

--- lib/chat/auth/user.ex
+++ lib/chat/auth/user.ex
@@ -1,9 +1,12 @@
 defmodule Chat.Auth.User do
   use Ecto.Schema
   import Ecto.Changeset
+  use Pow.Ecto.Schema
 
   schema "auth_users" do
     field(:nickname, :string)
+    field(:email, :string)
+    field(:password_hash, :string)
 
     timestamps()
   end
@@ -11,6 +14,7 @@ defmodule Chat.Auth.User do
   @doc false
   def changeset(user, attrs) do
     user
+    |> pow_changeset(attrs)
     |> cast(attrs, [:nickname])
     |> validate_required([:nickname])
     |> unique_constraint(:nickname)
